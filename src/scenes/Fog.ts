cacf99671c5193fdc49ec8fb5dc5f71b00a0d6b4
feng <908087098@qq.com>
2019-12-18 21:33:53 +0800

    Fog

---
+++ src/scenes/Fog.ts
@@ -0,0 +1,46 @@
+namespace THREE
+{
+
+	/**
+	 * @author mrdoob / http://mrdoob.com/
+	 * @author alteredq / http://alteredqualia.com/
+	 */
+
+	export function Fog(color, near, far)
+	{
+
+		this.name = '';
+
+		this.color = new Color(color);
+
+		this.near = (near !== undefined) ? near : 1;
+		this.far = (far !== undefined) ? far : 1000;
+
+	}
+
+	Object.assign(Fog.prototype, {
+
+		isFog: true,
+
+		clone: function ()
+		{
+
+			return new Fog(this.color, this.near, this.far);
+
+		},
+
+		toJSON: function ( /* meta */)
+		{
+
+			return {
+				type: 'Fog',
+				color: this.color.getHex(),
+				near: this.near,
+				far: this.far
+			};
+
+		}
+
+	});
+
+}
