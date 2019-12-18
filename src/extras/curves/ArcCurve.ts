563a67a813e3537157e272c99e62ce8b2af052fb
feng <908087098@qq.com>
2019-12-18 23:28:29 +0800

    curves

---
+++ src/extras/curves/ArcCurve.ts
@@ -0,0 +1,20 @@
+namespace THREE
+{
+
+
+	export function ArcCurve(aX, aY, aRadius, aStartAngle, aEndAngle, aClockwise)
+	{
+
+		EllipseCurve.call(this, aX, aY, aRadius, aRadius, aStartAngle, aEndAngle, aClockwise);
+
+		this.type = 'ArcCurve';
+
+	}
+
+	ArcCurve.prototype = Object.create(EllipseCurve.prototype);
+	ArcCurve.prototype.constructor = ArcCurve;
+
+	ArcCurve.prototype.isArcCurve = true;
+
+
+}
